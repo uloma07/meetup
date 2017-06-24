700fd317a3f73dfdabba9536694d4304323b3209
Oge O <oge.blessing@gmail.com>
2017-06-24 21:50:51 +0100

    First commit

---
+++ meetup_members.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python3
+
+import requests
+import json
+import sys
+
+api_url = 'https://api.meetup.com/2/'
+
+
+
+def main(argv):
+
+    page_size = 200
+
+    api_key = argv[0]
+    group_urlname = argv[1]
+    if(len(argv) > 2):
+        page_size = argv[2]
+    
+    get_members(api_key, group_urlname, page_size)
+    #get_user_groups(api_key, group_urlname , page_size)
+
+
+
+def get_members(api_key, group_urlname, page_size=200):
+
+    results = None
+    users = []
+    query = 'members'
+
+    request_string = '{}{}?key={}&group_urlname={}&page={}'.format(api_url, query, api_key, group_urlname, page_size)
+
+    while True:
+
+        # print(request_string)
+
+        r = requests.get(request_string)
+        try:
+            results = json.loads(r.content.decode('utf-8'))
+        except Exception as e:
+            print(e)
+
+        # print(results)
+
+        num = len(results['results'])
+        users += results['results']
+
+        # print(num)
+
+        try:
+            if len(results['meta']['next']) <= 0:
+                break
+        except e:
+            print(e)
+            break
+
+        request_string = results['meta']['next']
+
+    print(json.dumps(users, indent=2))
+
+
+def get_user_groups(api_key, member_id , page_size=200):
+    
+    results = None
+    groups = []
+    query = 'groups'
+    request_string = f'{api_url}{query}?key={api_key}&member_id={member_id}&page={page_size}&only=id,link,rating,name,category.name,topics,members,topics.urlkey'
+
+    while True:
+
+        # print(request_string)
+
+        r = requests.get(request_string)
+        try:
+            results = json.loads(r.content.decode('utf-8'))
+        except Exception as e:
+            print(e)
+
+        # print(results)
+
+        num = len(results['results'])
+        groups += results['results']
+
+        # print(num)
+
+        try:
+            if len(results['meta']['next']) <= 0:
+                break
+        except e:
+            print(e)
+            break
+
+        request_string = results['meta']['next']
+
+    print(json.dumps(groups, indent=2))
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
