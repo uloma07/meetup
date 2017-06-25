98349572092e923e4fb960e8fcac99517b8152a0
Akinkunle Allen <allenakinkunle@Akinkunles-MacBook-Pro.local>
2017-06-25 16:15:47 +0100

    Add Bubble chart

--- bubble.js
+++ bubble.js
@@ -1,26 +1,28 @@
-var bubbleChart = function () {
-    var width = 960,
-    height = 960;
+var bleed = 100,
+    width = 960,
+    height = 760;
 
-    function chart(selection){
-        // you gonna get here
-    }
+var svg = d3.select("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .append("g")
+    .attr("transform", "translate(0,0)");
 
-    chart.width = function(value) {
-        if (!arguments.length) { return width }
-        width = value;
-        return chart
-    };
+d3.csv("result.csv", function(error, data) {
+    if (error) throw error
+    var node = svg.selectAll(".node")
+        .data(data)
+        .enter().append("g")
+        .attr("class", "node")
+        .attr("transform", function(d) { return "translate(" + Math.floor((Math.random() * 670) + 30) + "," + Math.floor((Math.random() * 870) + 30) + ")"; })
 
-    chart.height = function(value) {
-        if (!arguments.length) { return height }
-        height = value;
-        return chart
-    };
+node.append("circle")
+    .attr("r", function(d) { return d.Count * 30 })
+    .style("background-color", "red");
 
-    return chart;
-};
-
-d3.json('https://raw.githubusercontent.com/uloma07/meetup/master/data.json', function(data) {
-    console.log(data);
+node.append("text")
+    .text(function(d) { return d.Name; })
+    .style("font-size", function(d) { "24px"; })
+    .style("color", function(d) { "#fff"; })
+    .attr("dy", ".35em");
 });
